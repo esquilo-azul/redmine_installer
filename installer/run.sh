bab6e7faba7312100c8c0f3f77aae2b9a64be883
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2018-07-31 18:34:31 -0300

    Settings support.

--- installer/run.sh
+++ installer/run.sh
@@ -5,6 +5,13 @@ set -e
 
 export INSTALL_ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 export PLUGIN_ROOT=$(dirname "$INSTALL_ROOT")
+export REDMINE_ROOT=$(dirname "$(dirname "$PLUGIN_ROOT")")
+
+source "$INSTALL_ROOT/default-settings.sh"
+APP_SETTINGS="$REDMINE_ROOT/config/install.sh"
+if [ -f "$APP_SETTINGS" ]; then
+  source "$APP_SETTINGS"
+fi
 
 function programeiro {
   PPATH="$INSTALL_ROOT/lib" "$PLUGIN_ROOT/vendor/programeiro/run.sh" "$@"
