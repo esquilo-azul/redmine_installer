483148885443228b93d302f0917e69150d7a38ec
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2018-07-31 18:20:04 -0300

    Plugin "redmine_installer".

---
+++ init.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'redmine'
+
+Redmine::Plugin.register :redmine_installer do
+  name 'Redmine Installer'
+  author 'Eduardo Henrique Bogoni'
+  description 'Installer for Redmine'
+  version '0.0.0'
+end
