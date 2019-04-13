ce1eb7e28fee69629e44d31eb16efd4fc7653630
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-04-13 10:25:39 -0300

    Plugin urls.

--- init.rb
+++ init.rb
@@ -5,6 +5,8 @@ require 'redmine'
 Redmine::Plugin.register :redmine_installer do
   name 'Redmine Installer'
   author 'Eduardo Henrique Bogoni'
-  description 'Installer for Redmine'
+  description 'Installer for Redmine.'
+  url 'https://github.com/esquilo-azul/redmine_installer'
+  author_url 'https://github.com/eduardobogoni/'
   version '0.1.0'
 end
