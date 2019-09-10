3a2a77c9dae8af7a162d1eb98259aad1b9190731
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-09-10 13:01:24 -0300

    Installer: program "/redmine/migration_status" checks plugins' migrations.

--- installer/programs/redmine/migration_status.sh
+++ installer/programs/redmine/migration_status.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-set +e
+set -e
+set -u
 
-function migration_status {
-  programeiro /rails/rake db:migrate:status 2> /dev/null
+function migration_status() {
+  local content="$(programeiro /rails/rake "$1" 2> /dev/null)"
+  set +e
+  echo "$content" | grep '^\s*down\s' > /dev/null 2> /dev/null
+  down=$?
+  echo "$content" | grep '^\s*up\s' > /dev/null 2> /dev/null
+  up=$?
+  set -e
+  if [ -z "$content" ]; then
+    echo 0
+  elif [ "$down" -eq 0 -o "$up" -ne 0 ]; then
+    echo 1
+  else
+    echo 0
+  fi
 }
 
-migration_status | grep '^\s*down\s' > /dev/null 2> /dev/null
-down=$?
-migration_status | grep '^\s*up\s' > /dev/null 2> /dev/null
-up=$?
-if [ "$down" -eq 0 -o "$up" -ne 0 ]; then
+if [ "$(migration_status 'db:migrate:status')" != '0' ]; then
   exit 1
-else
-  exit 0
 fi
+if [ "$(migration_status 'redmine:plugins:migrate:status')" != '0' ]; then
+  exit 1
+fi
+exit 0
