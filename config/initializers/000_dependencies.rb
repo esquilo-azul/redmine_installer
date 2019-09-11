53031130e18792872ecd3e5a6b94d43179dd950b
Eduardo Henrique Bogoni <eduardo.bogoni@trf1.jus.br>
2019-09-11 14:53:38 -0300

    Plugin "redmine_plugins_helper" '>= 0.5.2'.

---
+++ config/initializers/000_dependencies.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+Redmine::Plugin.post_register :redmine_installer do
+  # Source: https://github.com/esquilo-azul/redmine_plugins_helper
+  requires_redmine_plugin(:redmine_plugins_helper, version_or_higher: '0.5.2')
+end
