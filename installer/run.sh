7a6ea84fb3313a441a076cd9ca120c3afade30b9
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-09-11 09:40:18 -0300

    Installer: extract "environment.sh" from "run.sh".

--- installer/run.sh
+++ installer/run.sh
@@ -3,21 +3,14 @@
 set -u
 set -e
 
-export INSTALL_ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-export PLUGIN_ROOT=$(dirname "$INSTALL_ROOT")
-export REDMINE_ROOT=$(dirname "$(dirname "$PLUGIN_ROOT")")
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
-source "$INSTALL_ROOT/default-settings.sh"
-APP_SETTINGS="$REDMINE_ROOT/config/install.sh"
-if [ -f "$APP_SETTINGS" ]; then
-  source "$APP_SETTINGS"
-fi
-
-function programeiro {
-  PPATH="$INSTALL_ROOT/lib" "$PLUGIN_ROOT/vendor/programeiro/run.sh" "$@"
-}
-export -f programeiro
-
-source "$INSTALL_ROOT/lib/rvm/source.sh"
+source "${DIR}/environment.sh"
 "$PLUGIN_ROOT/vendor/taskeiro/taskeiro" --path "$INSTALL_ROOT/tasks" "$@"
 programeiro /redmine/installer/triggers/run_all
