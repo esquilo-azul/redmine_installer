1a4d4f31d3dbf6f0f005e8f1395892d964a9b357
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-11-12 17:26:16 -0300

    installer/tasks/redmine_configuration: support authentication "none".

--- installer/default_settings.sh
+++ installer/default_settings.sh
@@ -10,6 +10,6 @@ export smtp_tls=false
 export smtp_server=localhost
 export smtp_domain=example.com
 export smtp_port=25
-export smtp_authentication=:plain
+export smtp_authentication=plain
 export smtp_username=redmine
 export smtp_password=redmine
