5d471af4c4b848f7a0cdaccc8dd654e03473cd10
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-05-12 15:53:15 -0300

    installer/tasks/libpng_twelve: use "curl".

--- installer/tasks/libpng_twelve.sh
+++ installer/tasks/libpng_twelve.sh
@@ -7,9 +7,13 @@ function task_condition {
   return $(programeiro /apt/installed libpng12-0)
 }
 
+function task_dependencies {
+  printf 'curl'
+}
+
 function task_fix {
   PACKAGE="$(mktemp -d)/libpng12.deb"
-  wget -q -O "$PACKAGE" http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb
+  curl -Lo "$PACKAGE" http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb
   sudo dpkg -i "$PACKAGE"
   rm "$PACKAGE"
 }
