84d09629215bd1225523c9f423bcbf203675070a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-07-26 17:08:28 -0300

    Bump to '0.11.2'.

--- init.rb
+++ init.rb
@@ -8,5 +8,5 @@ Redmine::Plugin.register :redmine_installer do
   description 'Installer for Redmine.'
   url 'https://github.com/esquilo-azul/redmine_installer'
   author_url 'https://github.com/eduardobogoni/'
-  version '0.11.1'
+  version '0.11.2'
 end
