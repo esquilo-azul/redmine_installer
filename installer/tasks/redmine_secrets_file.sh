fd2b9e7f1bd682f577e4a4550ef31ce768203fac
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-02-09 11:45:36 -0300

    installer/tasks/redmine_secrets_file: depend on "redmine_secrets_key" task.

--- installer/tasks/redmine_secrets_file.sh
+++ installer/tasks/redmine_secrets_file.sh
@@ -6,11 +6,22 @@ set -e
 TEMPLATE="$INSTALL_ROOT/template/redmine_secrets.yml"
 TARGET="$REDMINE_ROOT/config/secrets.yml"
 
+function task_dependencies {
+  echo redmine_secrets_key
+}
+
 function task_condition {
-  [ -f "$TARGET" ]
+  export secret_key_base="$(cat "$SECRET_KEY_PATH")"
+  if [ -z "$secret_key_base" ]; then
+    return 1
+  fi
+
+  programeiro /template/diff "$TEMPLATE" "$TARGET"
 }
 
 function task_fix {
-  export secret_key_base=$(programeiro /rails/generate_secret_key)
-  programeiro /template/apply "$TEMPLATE" "$TARGET"
+  export secret_key_base="$(cat "$SECRET_KEY_PATH")"
+  if [ -n "$secret_key_base" ]; then
+    programeiro /template/apply "$TEMPLATE" "$TARGET"
+  fi
 }
