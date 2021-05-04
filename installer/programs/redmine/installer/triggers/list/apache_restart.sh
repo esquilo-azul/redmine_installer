201a5623e7cfd2321c1689276d01bd9158ebd6be
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-05-04 16:38:27 -0300

    installer/triggers/list/apache_restart: try restart Apache only if its service is available.

--- installer/programs/redmine/installer/triggers/list/apache_restart.sh
+++ installer/programs/redmine/installer/triggers/list/apache_restart.sh
@@ -3,4 +3,9 @@
 set -u
 set -e
 
-sudo service apache2 restart
+SERVICE_NAME='apache2'
+if service --status-all | grep "$SERVICE_NAME" ; then
+  sudo service "$SERVICE_NAME" restart
+else
+  printf "Service \"%s\" is not installed\n" "$SERVICE_NAME"
+fi
