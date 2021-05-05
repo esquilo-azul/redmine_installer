56cfa72b7a0570094bef18f0956484124c77d86c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-05-04 23:55:57 -0300

    installer/tasks/libpng_twelve: remove no more necessary.

--- installer/tasks/development.sh
+++ installer/tasks/development.sh
@@ -8,6 +8,6 @@ function task_condition {
 }
 
 function task_dependencies {
-  echo libpng_twelve redmine_database_schema redmine_database_test redmine_host_name_setting \
+  echo redmine_database_schema redmine_database_test redmine_host_name_setting \
     redmine_mail_from_setting
 }
