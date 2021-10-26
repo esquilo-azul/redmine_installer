c8f841b72a1c11d5d88da3ae1e85fbac5bf103c3
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-10-26 13:50:44 -0300

    installer/programs/ruby/gem_install: allows define gem's version.

--- installer/programs/ruby/gem_install.sh
+++ installer/programs/ruby/gem_install.sh
@@ -3,4 +3,16 @@
 set -u
 set -e
 
-gem install -V --conservative "$@"
+NAME="$1"
+VERSION=''
+
+if [ $# -ge 2 ]; then
+  VERSION="$2"
+fi
+
+INSTALL_ARGS=("$NAME")
+if [ -n "$VERSION" ]; then
+  INSTALL_ARGS+=("-v=${VERSION}")
+fi
+
+gem install -V --conservative "${INSTALL_ARGS[@]}"
