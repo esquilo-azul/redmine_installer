8139546c1cc5697002b7b402d44bd7f17f627cd7
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-08-05 19:10:43 -0300

    vendor/eac-bash-lib: update to '0.13.0'.

--- vendor/eac-bash-lib/python.sh
+++ vendor/eac-bash-lib/python.sh
@@ -1,3 +1,5 @@
+export DEFAULT_PIP_COMMAND='pip'
+
 function python_assert_installed() {
   INSTALL=()
   for PKG in $@; do
@@ -8,17 +10,25 @@ function python_assert_installed() {
 
   if [ ${#INSTALL[@]} -gt 0 ]; then
     infom "Será necessário instalar os seguintes pacotes Python: $INSTALL"
-    sudo pip install "${INSTALL[@]}"
+    sudo "$(python_pip_command)" install "${INSTALL[@]}"
   fi
 }
 export -f python_assert_installed
 
 function python_installed() {
   for PKG in $@; do
-    RESULT="$(pip list | cut -d ' ' -f1 | grep "$PKG")"
+    RESULT="$("$(python_pip_command)" list | cut -d ' ' -f1 | grep "$PKG")"
     if [ "$RESULT" != "$PKG" ] ; then
       return 1
     fi
   done
 }
 export -f python_installed
+
+function python_pip_command() {
+  if var_present_r 'PIP_COMMAND'; then
+    outout "${PIP_COMMAND}\n"
+  else
+    outout "${DEFAULT_PIP_COMMAND}\n"
+  fi
+}
