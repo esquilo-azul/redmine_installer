bcb5a3122f3eebc8977bd3da5871bf62508a3a39
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-08-17 14:36:22 -0300

    installer/programs/redmine/installer/triggers/list/restart_application.sh: inform Apache's restart.

--- installer/programs/redmine/installer/triggers/list/restart_application.sh
+++ installer/programs/redmine/installer/triggers/list/restart_application.sh
@@ -4,6 +4,7 @@ set -u
 set -e
 
 function restart_apache() {
+  >&2 printf -- "%s\n" "Restarting Apache HTTPD..."
   SERVICE_NAME='apache2'
   if service --status-all | grep "$SERVICE_NAME" ; then
     sudo service "$SERVICE_NAME" restart
