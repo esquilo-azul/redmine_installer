b23d200d895e3ca0406b36de9d25d8f2dd255fd2
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-08-17 14:36:08 -0300

    installer/programs/redmine/installer/triggers/list/restart_application.sh: refact.

--- installer/programs/redmine/installer/triggers/list/restart_application.sh
+++ installer/programs/redmine/installer/triggers/list/restart_application.sh
@@ -3,9 +3,13 @@
 set -u
 set -e
 
-SERVICE_NAME='apache2'
-if service --status-all | grep "$SERVICE_NAME" ; then
-  sudo service "$SERVICE_NAME" restart
-else
-  printf "Service \"%s\" is not installed\n" "$SERVICE_NAME"
-fi
+function restart_apache() {
+  SERVICE_NAME='apache2'
+  if service --status-all | grep "$SERVICE_NAME" ; then
+    sudo service "$SERVICE_NAME" restart
+  else
+    printf "Service \"%s\" is not installed\n" "$SERVICE_NAME"
+  fi
+}
+
+restart_apache
