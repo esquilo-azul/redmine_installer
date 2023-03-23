4b00484ad9ad2d18aefad34cc2bd4a0b39a62eac
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-03-23 10:12:40 -0300

    _outerr(): extract from "speaker.sh"#*().

--- sub/taskeiro/lib/speaker.sh
+++ sub/taskeiro/lib/speaker.sh
@@ -7,21 +7,25 @@ export FG_LBLUE='\e[94m'
 export BG_RED='\e[41m'
 export NC='\033[0m' # No Color
 
+function _outerr() {
+  >&2 printf -- '%b' "$1"
+}
+
 function _fatal_error() {
-  >&2 printf "$BG_RED$1$NC\n"
+  _outerr "$BG_RED$1$NC\n"
   exit 1
 }
 
 function _success() {
-  >&2 printf "$FG_LGREEN$1$NC\n"
+  _outerr "$FG_LGREEN$1$NC\n"
 }
 
 function _infov() {
-  >&2 printf "$FG_CYAN$1:$NC $2\n"
+  _outerr "$FG_CYAN$1:$NC $2\n"
 }
 
 function _debug() {
   if [ -n "$TASKEIRO_DEBUG" ]; then
-    >&2 printf "${FG_LBLUE}${1}${NC}\n"
+    _outerr "${FG_LBLUE}${1}${NC}\n"
   fi
 }
