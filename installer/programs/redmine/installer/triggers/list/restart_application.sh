e2e373c5bc79675c7069b7bd79af97272ad8f39c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-03-26 00:20:31 -0300

    installer/programs/redmine/installer/triggers/list/restart_application.sh: use infom().

--- installer/programs/redmine/installer/triggers/list/restart_application.sh
+++ installer/programs/redmine/installer/triggers/list/restart_application.sh
@@ -4,17 +4,17 @@ set -u
 set -e
 
 function restart_apache() {
-  >&2 printf -- "%s\n" "Restarting Apache HTTPD..."
+  infom "Restarting Apache HTTPD..."
   SERVICE_NAME='apache2'
   if service --status-all | grep "$SERVICE_NAME" ; then
     sudo service "$SERVICE_NAME" restart
   else
-    printf "Service \"%s\" is not installed\n" "$SERVICE_NAME"
+    infom "Service \"${$SERVICE_NAME}\" is not installed"
   fi
 }
 
 function generate_plugins_assets() {
-  >&2 printf -- "%s\n" "Generating plugins assets..."
+  infom "Generating plugins assets..."
   RAILS_ENV=production programeiro /rails/rake redmine:plugins:assets
 }
 
