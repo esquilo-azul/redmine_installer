9f9fb05122bf880e995c01bca50e0ac1112cf1e9
Eduardo Henrique Bogoni <eduardo.bogoni@trf1.jus.br>
2025-07-03 13:28:50 -0300

    Installer#Task[passenger]: fix.

--- installer/tasks/passenger.sh
+++ installer/tasks/passenger.sh
@@ -14,5 +14,8 @@ function task_condition {
 }
 
 function task_fix {
-  package_assert ruby "$GEM_NAME"
+  local TEMP_DIR="$(mktemp -d)"
+  local TEMP_GEMFILE="${TEMP_DIR}/Gemfile"
+  cp "${INSTALL_ROOT}/template/passenger_gemfile" "$TEMP_GEMFILE"
+  BUNDLE_GEMFILE="$TEMP_GEMFILE" bundle install
 }
