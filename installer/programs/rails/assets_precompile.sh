7812ea4f1323f7acef57257c5af5417017a397c0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-08-13 15:51:26 -0300

    Program[rails/assets_precompile]: exit with error if Node is not found.

--- installer/programs/rails/assets_precompile.sh
+++ installer/programs/rails/assets_precompile.sh
@@ -7,4 +7,6 @@ if node --version; then
   infom "Generating plugins assets..."
   RAILS_ENV=production RAILS_RELATIVE_URL_ROOT="${address_path}" programeiro /rails/rails assets:clean \
     assets:precompile
+else
+  fatal_error "Node not found - will not be possible to compile assets"
 fi
